708222431b85c7ebf1056fbb7297f88ccdc6a3bd
Ankit-Developer-2024 <kumar990ankit@gmail.com>
2025-08-26 00:05:43 +0530

    user address upate and add both done

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,46 +1,146 @@
-const { User } = require("../models/user_model")
+const { User } = require("../models/user_model");
 
-exports.fetchUser=async(req,res)=>{
-    try {
-        let {id}=req.user
+exports.fetchUser = async (req, res) => {
+  try {
+    let { id } = req.user;
 
-        let user=await User.findById(id);
-        if (user) {
-             let userData={
-                name:user.name,
-                email:user.email,
-                dob:user.dob,
-                gender:user.gender,
-                phone:user.phone,
-                addresses:user.addresses,
-                image:user.image,
-                role:user.role
-              } 
-         res.status(200).json({"message":"User fetch successfully","success":true,"rs":200,"data":userData})
-        } else{
-         res.status(400).json({"message":"User not found","success":true,"rs":400,"data":null})
-        }       
-    } catch (error) {
-        res.status(500).json({"message":String(error),"success":false,"rs":500,"data":null})
+    let user = await User.findById(id);
+    if (user) {
+      let userData = {
+        name: user.name,
+        email: user.email,
+        dob: user.dob,
+        gender: user.gender,
+        phone: user.phone,
+        addresses: user.addresses,
+        image: user.image,
+        role: user.role,
+      };
+      res
+        .status(200)
+        .json({
+          message: "User fetch successfully",
+          success: true,
+          rs: 200,
+          data: userData,
+        });
+    } else {
+      res
+        .status(400)
+        .json({
+          message: "User not found",
+          success: true,
+          rs: 400,
+          data: null,
+        });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: String(error), success: false, rs: 500, data: null });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    let { id } = req.user;
+    let { addresses } = req.body;
+    if (addresses) {
+      let user = await User.findById(id);
+      user.addresses.push(addresses);
+      user = await User.findOneAndUpdate(
+        { _id: id },
+        { $set: { addresses: user.addresses } },
+        { new: true, runValidators: true }
+      );
+      let userData = {
+        name: user.name,
+        email: user.email,
+        dob: user.dob,
+        gender: user.gender,
+        addresses: user.addresses,
+        phone: user.phone,
+        role: user.role,
+      };
+      res.status(200) .json({
+          message: "User updated successfully",
+          success: true,
+          rs: 200,
+          data: userData,
+        });
+    } else {
+      let user = await User.findByIdAndUpdate( id,{ ...req.body },{ new: true });
+      let userData = {
+        name: user.name,
+        email: user.email,
+        dob: user.dob,
+        gender: user.gender,
+        addresses: addresses,
+        phone: user.phone,
+        role: user.role,
+      };
+      res
+        .status(200)
+        .json({
+          message: "User updated successfully",
+          success: true,
+          rs: 200,
+          data: userData,
+        });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: String(error), success: false, rs: 500, data: null });
+  }
+};
+
+exports.updateUserAddress = async (req, res) => {
+  try {
+    let { id } = req.user; 
+    let user = await User.findById(id);
+     user.addresses.forEach((item,i)=>{
+      if(item.id===req.params.id){ 
+        item.houseNo=req.body.addresses.houseNo 
+        item.street=req.body.addresses.street 
+        item.city=req.body.addresses.city 
+        item.landMark=req.body.addresses.landMark 
+        item.state=req.body.addresses.state 
+        item.zipcode=req.body.addresses.zipcode 
+        item.country=req.body.addresses.country 
+        item.isDefault=req.body.addresses.isDefault
+      }else if(req.body.addresses.isDefault===true){ 
+         item.isDefault=false
       }
-}
-
-exports.updateUser=async(req,res)=>{
-     try {
-        let {id}=req.user 
-         let user=await User.findByIdAndUpdate(id,{...req.body},{new:true});
-         let userData={
-                name:user.name,
-                email:user.email,
-                dob:user.dob,
-                gender:user.gender,
-                phone:user.phone,
-                role:user.role
-              }
-         res.status(200).json({"message":"User updated successfully","success":true,"rs":200,"data":userData})
-     } catch (error) {
-           res.status(500).json({"message":String(error),"success":false,"rs":500,"data":null})
-     }
-
-
-}
+     })  
+     user = await User.findOneAndUpdate(
+        { _id: id },
+        { $set: { addresses: user.addresses } },
+        { new: true, runValidators: true }
+      );
+      
+    let userData = {
+      name: user.name,
+      email: user.email,
+      dob: user.dob,
+      gender: user.gender,
+      addresses: user.addresses,
+      phone: user.phone,
+      role: user.role,
+    };
+    res
+      .status(200)
+      .json({
+        message: "User updated -successfully",
+        success: true,
+        rs: 200,
+        data: userData,
+      });
+  } catch (error) {
+    console.log(error);
+    
+    res
+      .status(500)
+      .json({ message: String(error), success: false, rs: 500, data: null });
+  }
+};
--- models/user_model.js
+++ models/user_model.js
@@ -2,13 +2,29 @@ const mongoose=require('mongoose')
 const bcrypt = require('bcryptjs'); 
 
 const addressSchema = new mongoose.Schema({
+  houseNo: { type: String, required: true },
   street: { type: String, required: true },
   city: { type: String, required: true },
+  landMark:{ type: String, required: true },
   state: { type: String, required: true },
   zipCode: { type: String, required: true },
   country: { type: String, required: true },
   isDefault: { type: Boolean, default: false } // Indicates if this is the default address
 });
+
+
+addressSchema.virtual('id').get(function () {
+    return this._id;
+})
+
+addressSchema.set('toJSON',{
+    virtuals:true,
+    versionKey:false,
+    transform(doc,ret){
+        delete ret._id
+    }
+})
+
 
 const userSchema=new mongoose.Schema({
     name:{type:String,require:true},
@@ -25,11 +41,51 @@ const userSchema=new mongoose.Schema({
 
 },{timestamps:true})
 
-userSchema.pre('save', async function() {
+userSchema.pre('save', async function(next) {  
+    if(!this.isModified('password'))  return next();
     const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash( this.password, salt)
-    this.password=hash
+    const hash = await bcrypt.hash(this.password, salt)
+    this.password=hash 
 });
+
+userSchema.pre('findOneAndUpdate',async function () { 
+    let addr=this.getUpdate() ||{}   
+    if(addr.$set.addresses[addr.$set.addresses.length-1].isDefault===true){ 
+        addr.$set.addresses.forEach((address, index) => {
+                if (addr.$set.addresses[addr.$set.addresses.length-1] !== address) {
+                    address.isDefault = false;
+                }
+            });   
+        return ;      
+    }
+    
+    let defaultAddressCount = 0;
+    let defaultAddressIndex = -1;
+
+     addr.$set.addresses.forEach((item,index)=>{
+         if(item.isDefault){
+           defaultAddressCount++;
+           defaultAddressIndex=index; 
+         }
+    })
+
+    if(defaultAddressCount===1) return;
+
+    if(defaultAddressCount>1){
+         addr.$set.addresses.forEach((address, index) => {
+                if (index !== defaultAddressIndex) {
+                    address.isDefault = false;
+                }
+            });
+    }else if(defaultAddressCount==0 && addr.$set.addresses.length>0){ 
+        addr.$set.addresses[0].isDefault=true;
+    }
+    
+})
+
+
+ 
+ 
 
 userSchema.virtual('id').get(function () {
     return this._id;
