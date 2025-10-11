1b77d1b2b22877ef31f2e549414a99e8a777b224
Ankit-Developer-2024 <kumar990ankit@gmail.com>
2025-10-11 17:08:39 +0530

    update index.js with serverless

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const categoryRouter =  require('./routers/category_router')
 const cartRouter =  require('./routers/cart_router')
 const orderRouter =  require('./routers/order_router')
 const passport = require('./services/passportService');   
-const port = 3000;
+//const port = 3000;
+const serverless = require("serverless-http");
 
 const corsOptions = {
   origin: 'http://localhost:5173', // Allow only your frontend's origin
@@ -31,8 +32,8 @@ app.use('/cart', passport.authenticate('jwt',{ session: false }),cartRouter.rout
 app.use('/order',passport.authenticate('jwt',{ session: false }),orderRouter.router)
 
 
-app.listen(port,()=>{
-    console.log("App run on",port)
-})  
+// app.listen(port,()=>{
+//     console.log("App run on",port)
+// })  
 
-module.exports=app;
+module.exports = serverless(app);
