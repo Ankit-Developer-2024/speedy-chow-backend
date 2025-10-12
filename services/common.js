ad9600280c442e9eacc1da6caf1d4da7cd53a8c8
Ankit-Developer-2024 <kumar990ankit@gmail.com>
2025-10-12 15:27:34 +0530

    log the cookie

--- services/common.js
+++ services/common.js
@@ -6,11 +6,13 @@ export const sanitizeUser = function sanitizeUser(user) {
 }
 
 export const cookieHearderExtractor=(req,res)=>{ 
-    
+     
+    console.log('cookies:--', req.cookies);
        if(req.headers.authorization){ 
         return req.headers.authorization.split(" ")[1]
        }
        if(req.cookies){    
+
          return  req.cookies.accessToken
        } 
        return ""
